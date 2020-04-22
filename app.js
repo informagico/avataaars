66f8c571f58332b5fceaa3eaaa89e39e3a22a882
informagico <alessandro.magoga92@gmail.com>
2020-04-22 19:53:21 +0200

    Cleanup, package update, removed storage on AWS and use local storage instead.

--- app.js
+++ app.js
@@ -1,78 +1,69 @@
-import express from 'express';
-const { convert } = require('convert-svg-to-png');
-
-const aws = require('./utils/aws');
-
-var crypto = require('crypto');
+import express from 'express'
+const { convert } = require('convert-svg-to-png')
+var crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
 
 // React Components
-import React from 'react';
-import RDS from 'react-dom/server';
-import Avataaars from 'avataaars';
+import React from 'react'
+import RDS from 'react-dom/server'
+import Avataaars from 'avataaars'
 
-const app = express();
+const app = express()
 
 app.get('/', async (req, res) => {
-  const appString = RDS.renderToString(<Avataaars {...req.query} />);
+	const appString = RDS.renderToString(<Avataaars {...req.query} />)
 
-  res.writeHead(200, {
-    'Content-Type': 'image/svg+xml',
-  });
-  res.end(appString);
-});
+	res.writeHead(200, {
+		'Content-Type': 'image/svg+xml',
+	})
+	res.end(appString)
+})
 
 const getHash = (req) => {
-  return crypto.createHash('md5').update(req.path + "-" + JSON.stringify(req.query)).digest('hex');
-};
+	return crypto.createHash('md5').update(req.path + '-' + JSON.stringify(req.query)).digest('hex')
+}
 
 app.get('/png/:width?', async (req, res) => {
-  const hash = getHash(req);
-  const fileName = `${getHash(req)}.png`;
+	const hash = getHash(req)
 
-  res.set('Content-Type', 'image/png');
+	const fileFolder = path.resolve(__dirname, 'generated')
+	const fileName = `${hash}.png`
+	const filePath = path.resolve(fileFolder, fileName)
 
-  aws.getObject(fileName, async (err, data) => {
-    if (data) {
-      console.log('Existing avatar found');
-      return res.end(data.Body);
-    }
+	res.set('Content-Type', 'image/png')
 
-    const appString = RDS.renderToString(<Avataaars {...req.query} />);
+	fs.readFile(filePath, async (err, data) => {
+		if (err) {
+			console.log('Generating new avatar')
 
-    const png = await convert(appString, {
-      width: parseInt(req.params.width || 500, 10),
-      puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      },
-    });
+			const appString = RDS.renderToString(<Avataaars {...req.query} />)
 
-    console.log('Generating new avatar');
-    aws.uploadFile(fileName, png, () => {
-      res.end(png);
-    });
-  });
+			const png = await convert(appString, {
+				width: parseInt(req.params.width || 500, 10),
+				puppeteer: {
+					args: [ '--no-sandbox', '--disable-setuid-sandbox' ],
+				},
+			})
 
-});
+			if (!await fs.existsSync(fileFolder)) await fs.mkdirSync(fileFolder, '0777', true)
+
+			await fs.writeFileSync(filePath, png, 'binary')
+
+			res.end(png)
+			return
+		}
+
+		console.log(`Returning pre-generated file [${fileName}]`)
+		res.end(data)
+	})
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+	let err = new Error('Not Found')
+	err.status = 404
+	next(err)
+})
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
-}
-
-module.exports = app;
+module.exports = app
