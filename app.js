4425e057d3a507f4dda652f07de10e0eb9ed648b
informagico <alessandro.magoga92@gmail.com>
2020-04-28 18:12:04 +0200

    Removed unusefull 404 handling. Update packages.

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ app.get('/png/:width?', async (req, res) => {
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	let err = new Error('Not Found')
-	err.status = 404
-	next(err)
-})
+// app.use(function(req, res, next) {
+// 	let err = new Error('Not Found')
+// 	err.status = 404
+// 	next(err)
+// })
 
 module.exports = app
